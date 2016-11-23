258f4a28c6a15860eb839770565e58b668382974
mememori <charlotte.elixir@gmail.com>
2016-11-23 19:07:36 -0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,32 +1,38 @@
 defmodule Arand.Mixfile do
+
   use Mix.Project
 
   def project do
-    [app: :arand,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :arand,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      preferred_cli_env: [espec: :test],
+      name: "Arand",
+      description: "Library to efficiently generate random data for testing",
+      package: package(),
+      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:earmark, "~> 1.0", only: :dev},
+      {:ex_doc, "~> 0.14", only: :dev},
+      {:credo, "~> 0.5", only: :dev},
+      {:dialyze, "~> 0.2", only: :dev},
+      {:espec, "~> 1.1.2", only: :test}]
+  end
+
+  defp package do
+    [
+      files: ~w/mix.exs lib README.md/,
+      maintainers: ["Charlotte Lorelei Oliveira"],
+      links: %{"Github" => "https://github.com/mememori/arand"}]
   end
 end
